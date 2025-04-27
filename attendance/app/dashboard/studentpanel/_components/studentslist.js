28860ffcfbca7e083f772aff610b5551dc2c6714
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-27 12:10:32 +0500

    read data

--- attendance/app/dashboard/studentpanel/_components/studentslist.js
+++ attendance/app/dashboard/studentpanel/_components/studentslist.js
@@ -10,7 +10,7 @@ const StudentsList = () => {
             const studentData = await getDocs(collection(db, "Students"))
             const Data = studentData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
             setstudent(Data)
-            console.log("Runnig");
+            console.log(Data);
 
         } catch (error) {
             console.log("Error in fetching");
@@ -24,36 +24,61 @@ const StudentsList = () => {
 
         <div>
             <ul>
+                <h2>here are students of the list</h2>
                 {student.map((student, index) => (
+                    // console.log(student)
+
                     <li key={index} className="text-white">
                         {student.name}
                     </li>
                 ))}
             </ul>
-            <ul>
-                {student.map((student, index) => (
-                    <li key={index} className="text-white">
-                        {student.name}
-                    </li>
-                ))}
-            </ul>
-            <ul>
-                {student.map((student, index) => (
-                    <li key={index} className="text-white">
-                        {student.name}
-                    </li>
-                ))}
-            </ul>
-            <ul>
-                {student.map((student, index) => (
-                    <li key={index} className="text-white">
-                        {student.name}
-                    </li>
-                ))}
-            </ul>
+
         </div>
 
     )
 }
 
 export default StudentsList
+
+
+// "use client"
+// import { collection, getDocs } from 'firebase/firestore'
+// import db from '../../../../firebaseConfig'
+// import React, { useEffect, useState } from 'react'
+
+// const StudentsList = () => {
+//     const [student, setstudent] = useState([])
+
+//     const fetchData = async () => {
+//         try {
+//             const querySnapshot = await getDocs(collection(db, "Students"));
+//             const Data = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+//             setstudent(Data);
+//             console.log("Fetched Students: ", Data);
+//         } catch (error) {
+//             console.log("Error in fetching", error);
+//         }
+//     }
+
+//     useEffect(() => {
+//         fetchData()
+//     }, [])
+
+//     return (
+//         <div>
+//             <h3>students list</h3>
+//             {student.map((student, index) => {
+//                 console.log(student);
+//                 return (
+//                     <li key={student.id} className="text-white">
+//                         {student.name}
+//                     </li>
+//                 )
+//             })}
+
+//         </div>
+//     )
+// }
+
+// export default StudentsList
--- attendance/app/dashboard/studentpanel/page.js
+++ attendance/app/dashboard/studentpanel/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import AddStudent from './_components/addStudent'
+// import AddStudent from './_components/addStudent'
 import StudentsList from './_components/studentslist'
 
 const StudentPanel = () => {
     return (
         <div>
-            <AddStudent />
+            {/* <AddStudent /> */}
             <StudentsList />
         </div>
     )
